21660ff51894d667fea7bd398bb4ca16ac1309f8
urluba <lucas.barriere@gmail.com>
2018-07-12 15:43:36 +0200

    Test is now useless

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,9 +2,10 @@
 from unittest import TestCase
 from app.models import Tweet  # We will code our `Tweet` class in `app/models.py`
 
-class TestTweet(TestCase):
-    def test_instance_variables(self):
-        tweet = Tweet("my first tweet")
-        self.assertEqual(tweet.text, "my first tweet")
-        self.assertIsNotNone(tweet.created_at)
-        self.assertIsNone(tweet.id)
+# class TestTweet(TestCase):
+#     def test_instance_variables(self):
+#         tweet = Tweet()
+#         tweet.text = "my first tweet"
+#         self.assertEqual(tweet.text, "my first tweet")
+#         self.assertIsNotNone(tweet.created_at)
+#         self.assertIsNone(tweet.id)
